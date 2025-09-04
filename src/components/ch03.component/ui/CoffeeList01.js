268930b356fdbd33345df79b325c829e693ed804
bluesky <bluesky@example.com>
2025-09-04 15:46:41 +0900

    second commit

--- src/components/ch03.component/ui/CoffeeList01.js
+++ src/components/ch03.component/ui/CoffeeList01.js
@@ -1,6 +1,6 @@
 function App() {
     //    console.log('샘플');
-    const coffeeList = [
+    const CoffeeList = [
         { id: 1, name: "아메리카노", price: 5000, description: "에스프레소의 쓴 맛과 향을 좋아하는 사람들이 물을 추가해서 즐기는 음료로, 물과 에스프레소의 비율에 따라서 쓴 맛과 진하게 즐길 수 있습니다." },
         { id: 2, name: "카푸치노", price: 6000, description: "스팀밀크와 거품을 올린 것을 섞어 만든 이탈리아의 전통적인 커피 음료입니다." },
         { id: 3, name: "에스프레소", price: 3000, description: "이탈리아에서 탄생한 진한 맛의 커피 음료입니다. 뜨거운 물을 고압으로 강제로 통과시켜 만들어지는데, 이 때문에 일반적인 커피보다 맛이 진하고 향도 풍부합니다." },
@@ -28,7 +28,7 @@ function App() {
                 </tr>
             </thead>
             <tbody>
-                {coffeList.map((coffee) => (
+                {CoffeeList.map((coffee) => (
                     <tr key={coffee.id}>
                         <td>{coffee.id}</td>
                         <td>{coffee.name}</td>
